9681efeb6c2baf31ae8f4c9d045ac203776425a4
Toni <antonio.kamber@jobiqo.com>
2023-01-19 20:31:44 +0100

    getRandomPassword: Implement random pass generation

--- src/components/PasswordGenerator/utils/index.ts
+++ src/components/PasswordGenerator/utils/index.ts
@@ -1,7 +1,7 @@
 import { PasswordGeneratorOptionsType } from "../types";
 
 // Reference: https://en.wikipedia.org/wiki/ASCII#ASCII_printable_characters
-const charMap: { [K in PasswordGeneratorOptionsType]: number[][] } = {
+const optionsMap: { [K in PasswordGeneratorOptionsType]: number[][] } = {
   lowercase: [[97, 122]],
   uppercase: [[65, 90]],
   numbers: [[48, 57]],
@@ -23,25 +23,42 @@ export function getRandomPassword(
     let amountOfCharsToGenerate =
       index + 1 === selectedOptions.length
         ? passwordLength - passwordChars.length
-        : getRandInRange(
+        : getRandNumInRange(
             1,
             passwordLength - selectedOptions.length + 1 - passwordChars.length
           );
 
     while (amountOfCharsToGenerate-- > 0) {
-      passwordChars.push(getRandForOption(option));
+      passwordChars.push(getRandCharForOption(option));
     }
   });
 
-  return passwordChars.join("");
+  return shuffleArray(passwordChars).join("");
 }
 
-function getRandForOption(option: PasswordGeneratorOptionsType): string {
-  return option[0] + getRandInRange(1, 100);
+function getRandCharForOption(option: PasswordGeneratorOptionsType): string {
+  const optionAsciiCodes = optionsMap[option];
+  let optionAsciiCodesRangeStart = optionAsciiCodes[0][0],
+    optionAsciiCodesRangeEnd = optionAsciiCodes[0][1];
+
+  if (optionAsciiCodes.length > 1) {
+    const optionAsciiCodesRange =
+      optionAsciiCodes[getRandNumInRange(0, optionAsciiCodes.length - 1)];
+    optionAsciiCodesRangeStart = optionAsciiCodesRange[0];
+    optionAsciiCodesRangeEnd = optionAsciiCodesRange[1];
+  }
+
+  return String.fromCharCode(
+    getRandNumInRange(optionAsciiCodesRangeStart, optionAsciiCodesRangeEnd)
+  );
 }
 
-function getRandInRange(min: number, max: number) {
+function getRandNumInRange(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function shuffleArray<T>(arr: T[]) {
+  return arr.sort(() => 0.5 - Math.random());
 }
